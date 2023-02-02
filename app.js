02d739d90ef373cef70280d010cc2ebd4b68b2e5
Vikas-18 <vikaskohli18impossible@gmail.com>
2023-02-02 10:29:27 +0530

    changes made

--- app.js
+++ app.js
@@ -10,20 +10,22 @@ const port=8000;
 
 mongoose.connect("mongodb+srv://vikas18:Virat18*@cluster0.s255uxr.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-
 const complainSchema = new mongoose.Schema({ 
+    enrollmentnumber:String,
     name: String,
     phone: String,
+    hostel:String,
     email: String,
-    branch:String,
+    password:String,
+    confirmpassword:String,
     room: String,
     problem: String,
     comment: String
 });
 
-const signin=mongoose.model("sign",complainSchema);
+const student=mongoose.model("studentDetail",complainSchema);
 const complain = mongoose.model("hostel",complainSchema);
+const enroll1234=mongoose.model("enrollmentnumberdetail",complainSchema);
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'));  // For serving static files
@@ -58,51 +60,115 @@ app.get('/tasks',(req,res)=>{
     .catch((y)=>{
         console.log(y);
     })
+});
+
+app.get('/admin',(req,res)=>{
+      res.status(200).render("admin.pug");
 })
+
+app.post('/admin',(req,res)=>{
+      
+    console.log(req.body);
+
+    let hostel1;
+    if(req.key=="hmnd")
+      hostel1="Hostel-Mahanadi";
+    else if(req.body.key=="idwt")
+      hostel1="Hostel-Indrawati";
+    else if(req.body.key=="pgh")
+      hostel1="PG Hostel";
+    else if(req.body.key=="snth")
+      hostel1="Seonath";
+    else if(req.body.key=="mnpt")
+      hostel1="Hostel-Mainput";
+    else if(req.body.key=="ctkt")
+      hostel1="Hostel-Chitrakot";
+    else if(req.body.key=="mlhr")
+      hostel1="Hostel-Malhar";
+    else if(req.body.key=="ktmsr")
+      hostel1="Hostel-Kotumsar";
+    else if(req.body.key=="srpr")
+      hostel1="Hostel-Sirpur";
+
+      complain.find({hostel:hostel1})
+      .then((x)=>{
+          res.status(200).render('tasks.ejs',{x});
+          console.log(x);
+      })
+      .catch((y)=>{
+          console.log(y);
+      })
+    
+});
+
 
 
 let Filecontent = fs.readFileSync('./views/chatbot.html',"utf-8");
 
-app.post('/signin',(req,res)=>{
+app.post('/signin',async(req,res)=>{
 
-    var mydata=new signin(req.body);
-    mydata.save().then(()=>{
-        res.send(Filecontent);
-    }).catch(()=>{
-        res.status(400).send("<h1>Submission is unsuccessful! <br> Try Again</h1>")
-    })
-    emailsent=req.body;
-    // send email here
-    const transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'hostelchatbot544@gmail.com',
-            pass:'wqtnfhhjkyqymytx'
-      },
- });
+    try {
+        const enroll1=req.body.enrollmentnumber;
+        // const password=req.body.password;
  
- 
- const mailoptions = {
-       from:'hostelchatbot544@gmail.com',
-       to:`${emailsent.email}`,
-       subject:'Hostel Chat Bot Signing in',
-       text:`You have successfully sign in to Hostel Chat Bot App. `
- };
-  
- transporter.sendMail(mailoptions,function(err){
-     if(err){
-         console.log(err);
+        const user  = await enroll1234.findOne({enrollmentnumber:enroll1});
+        if(user==null){
+            res.send(`<script>alert("Please Enter valid username")</script>`);
+        }
+        else{
+            const p=req.body.password;
+            const cp=req.body.confirmpassword;
+            
+            if(p==cp){
+                var mydata=new student(req.body);
+                mydata.save().then(()=>{
+                    res.send(Filecontent);
+                }).catch(()=>{
+                    res.status(400).send("<h1>Submission is unsuccessful! <br> Try Again</h1>")
+                });
+
+                emailsent=req.body;
+                // send email here
+                const transporter = nodemailer.createTransport({
+                    service:'gmail',
+                    auth:{
+                        user:'hostelchatbot544@gmail.com',
+                        pass:'wqtnfhhjkyqymytx'
+                  },
+             });
+             
+             
+             const mailoptions = {
+                   from:'hostelchatbot544@gmail.com',
+                   to:`${emailsent.email}`,
+                   subject:'Hostel Chat Bot Signing in',
+                   text:`You have successfully sign in to Hostel Chat Bot App. `
+             };
+              
+             transporter.sendMail(mailoptions,function(err){
+                 if(err){
+                     console.log(err);
+                 }
+                 else{ 
+                     console.log('Message sent!!!');
+                 }
+             });
+            }
+            else {
+                res.send("passwords are not matching")
+            }
+        }
+     } catch (error) {
+         res.status(400).send(error);
      }
-     else{ 
-         console.log('Message sent!!!');
-     }
- });
-    console.log(req.body);
+     
+
+
+    
 });
 
-app.get('/email',(req,res)=>{
-    res.status(200).render('login.pug');
-});
+
+
 
 app.get('/', (req,res)=>{
     res.status(200).render('index.pug');
@@ -122,7 +188,34 @@ app.get('/chatbot', (req,res)=>{
 
 app.get('/complain', (req,res)=>{
      res.status(200).render('complain.pug');
-})
+});
+
+app.get('/login', (req,res)=>{
+     res.status(200).render('login.pug');
+});
+
+app.post("/login",async(req,res)=>{
+    try {
+       const enroll1=req.body.enrollmentnumber;
+       const password=req.body.password;
+
+       const user  = await student.findOne({enrollmentnumber:enroll1});
+       
+       if(password==user.password){
+           res.status(201).send(Filecontent);
+       }
+       else{
+           res.send(`<script>alert("Invalid Login details")</script>`);
+       }
+
+    } catch (error) {
+        res.status(400).send(`<script>alert("Invalid Login details")</script>`);
+    }
+});
+
+
+
+
 
 
 
--- static/JS/main.js
+++ static/JS/main.js
@@ -130,10 +130,13 @@ document.getElementById("navbar-toggle").addEventListener("click", function() {
   document.getElementById("signinbtn").addEventListener("click", function() {
     window.location.href = "/signin";
   });
-  document.getElementById("taskbtn").addEventListener("click", function() {
-    window.location.href = "/tasks";
+  document.getElementById("loginbtn").addEventListener("click", function() {
+    window.location.href = "/login";
   });
   document.getElementById("logoclick").addEventListener("click", function() {
     window.location.href = "/";
+  });
+  document.getElementById("adminbtn").addEventListener("click", function() {
+    window.location.href = "/admin";
   });
   
